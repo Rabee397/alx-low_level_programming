026a00bb96e55478a7050d28c0aede60382ecc51
rabee397.rabee <rabee397.rabie201097_1_11@hotmail.com>
2022-10-18 17:37:21 -0700

    task 2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,11 +1,9 @@
 #include "main.h"
-/*
- * *
+/**
  * *print_alphabet_x10 - prints alphabet letters
  * * ten times
  * *desc: The function uses loop to operate
  * *Return: no return
- *
  * */
 void print_alphabet_x10(void)
 {
