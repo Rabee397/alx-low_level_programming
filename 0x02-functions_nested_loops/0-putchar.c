86a51b6ab1d1c3922138fe8f82b4336aa8d5ae8a
rabee397.rabee <rabee397.rabie201097_1_11@hotmail.com>
2022-10-18 17:30:39 -0700

    task 1

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,20 +1,16 @@
 #include "main.h"
-
 /**
  * *main - function prints text as output
  * *
  * *Return:return 0
- * */
+ *
+ **/
 int main(void)
 {
 		char school[10] = "_putchar";
-
-			int i;
-
-				for (i = 0; i < 8; i++)
-						{
-									_putchar(school[i]);
-										}
-					_putchar(10);
-						return (0);
+		int i;
+		for (i = 0; i < 8; i++)
+				_putchar(school[i]);															_putchar(10);
+ 
+		return (0);
 }
